51a49a3a0e71efd27dc7a70103e09bf509718808
Mrinal Kanti Dhar <mrinal054@gmail.com>
2023-05-14 01:30:01 -0600

    Add files via upload

---
+++ segmentation_models_pytorch/utils/losses.py
@@ -0,0 +1,507 @@
+from typing import Optional
+
+import torch
+import torch.nn as nn
+from torch import Tensor
+from . import base
+from . import functional as F
+from ..base.modules import Activation
+
+
+class JaccardLoss(base.Loss):
+    def __init__(self, eps=1.0, activation=None, ignore_channels=None, **kwargs):
+        super().__init__(**kwargs)
+        self.eps = eps
+        self.activation = Activation(activation)
+        self.ignore_channels = ignore_channels
+
+    def forward(self, y_pr, y_gt):
+        y_pr = self.activation(y_pr)
+        return 1 - F.jaccard(
+            y_pr,
+            y_gt,
+            eps=self.eps,
+            threshold=None,
+            ignore_channels=self.ignore_channels,
+        )
+
+
+class DiceLoss(base.Loss):
+    def __init__(self, eps=1.0, beta=1.0, activation=None, ignore_channels=None, **kwargs):
+        super().__init__(**kwargs)
+        self.eps = eps
+        self.beta = beta
+        self.activation = Activation(activation)
+        self.ignore_channels = ignore_channels
+
+    def forward(self, y_pr, y_gt):
+        y_pr = self.activation(y_pr)
+        return 1 - F.f_score(
+            y_pr,
+            y_gt,
+            beta=self.beta,
+            eps=self.eps,
+            threshold=None,
+            ignore_channels=self.ignore_channels,
+        )
+
+
+class L1Loss(nn.L1Loss, base.Loss):
+    pass
+
+
+class MSELoss(nn.MSELoss, base.Loss):
+    pass
+
+
+class CrossEntropyLoss(nn.CrossEntropyLoss, base.Loss):
+    pass
+
+
+class NLLLoss(nn.NLLLoss, base.Loss):
+    pass
+
+
+class BCELoss(nn.BCELoss, base.Loss):
+    pass
+
+
+class BCEWithLogitsLoss(nn.BCEWithLogitsLoss, base.Loss): 
+    pass
+
+
+
+# =======================================================================
+import sys
+sys.path.append('segmentation_models_pytorch/losses')
+
+from typing import Optional
+from functools import partial
+
+import torch
+from torch.nn.modules.loss import _Loss
+from _functional import focal_loss_with_logits
+from constants import BINARY_MODE, MULTICLASS_MODE, MULTILABEL_MODE
+
+__all__ = ["FocalLoss"]
+
+
+class FocalLoss(_Loss, base.Loss):
+    def __init__(
+        self,
+        mode: str = 'binary',
+        alpha: Optional[float] = None,
+        gamma: Optional[float] = 2.0,
+        ignore_index: Optional[int] = None,
+        reduction: Optional[str] = "mean",
+        normalized: bool = False,
+        reduced_threshold: Optional[float] = None,
+    ):
+        """Compute Focal loss
+        Args:
+            mode: Loss mode 'binary', 'multiclass' or 'multilabel'
+            alpha: Prior probability of having positive value in target.
+            gamma: Power factor for dampening weight (focal strength).
+            ignore_index: If not None, targets may contain values to be ignored.
+                Target values equal to ignore_index will be ignored from loss computation.
+            normalized: Compute normalized focal loss (https://arxiv.org/pdf/1909.07829.pdf).
+            reduced_threshold: Switch to reduced focal loss. Note, when using this mode you
+                should use `reduction="sum"`.
+        Shape
+             - **y_pred** - torch.Tensor of shape (N, C, H, W)
+             - **y_true** - torch.Tensor of shape (N, H, W) or (N, C, H, W)
+        Reference
+            https://github.com/BloodAxe/pytorch-toolbelt
+        """
+        assert mode in {BINARY_MODE, MULTILABEL_MODE, MULTICLASS_MODE}
+        super().__init__()
+
+        self.mode = mode
+        self.ignore_index = ignore_index
+        self.focal_loss_fn = partial(
+            focal_loss_with_logits,
+            alpha=alpha,
+            gamma=gamma,
+            reduced_threshold=reduced_threshold,
+            reduction=reduction,
+            normalized=normalized,
+        )
+
+    def forward(self, y_pred: torch.Tensor, y_true: torch.Tensor) -> torch.Tensor:
+
+        if self.mode in {BINARY_MODE, MULTILABEL_MODE}:
+            y_true = y_true.view(-1)
+            y_pred = y_pred.view(-1)
+
+            if self.ignore_index is not None:
+                # Filter predictions with ignore label from loss computation
+                not_ignored = y_true != self.ignore_index
+                y_pred = y_pred[not_ignored]
+                y_true = y_true[not_ignored]
+
+            loss = self.focal_loss_fn(y_pred, y_true)
+
+        elif self.mode == MULTICLASS_MODE:
+
+            num_classes = y_pred.size(1)
+            loss = 0
+
+            # Filter anchors with -1 label from loss computation
+            if self.ignore_index is not None:
+                not_ignored = y_true != self.ignore_index
+
+            for cls in range(num_classes):
+                cls_y_true = (y_true == cls).long()
+                cls_y_pred = y_pred[:, cls, ...]
+
+                if self.ignore_index is not None:
+                    cls_y_true = cls_y_true[not_ignored]
+                    cls_y_pred = cls_y_pred[not_ignored]
+
+                loss += self.focal_loss_fn(cls_y_pred, cls_y_true)
+
+        return loss
+
+
+# =======================================================================
+# MCC loss
+class MCCLoss(_Loss, base.Loss):
+    def __init__(self, eps: float = 1e-5):
+        """Compute Matthews Correlation Coefficient Loss for image segmentation task.
+        It only supports binary mode.
+        Args:
+            eps (float): Small epsilon to handle situations where all the samples in the dataset belong to one class
+        Reference:
+            https://github.com/kakumarabhishek/MCC-Loss
+        """
+        super().__init__()
+        self.eps = eps
+
+    def forward(self, y_pred: torch.Tensor, y_true: torch.Tensor) -> torch.Tensor:
+        """Compute MCC loss
+        Args:
+            y_pred (torch.Tensor): model prediction of shape (N, H, W) or (N, 1, H, W)
+            y_true (torch.Tensor): ground truth labels of shape (N, H, W) or (N, 1, H, W)
+        Returns:
+            torch.Tensor: loss value (1 - mcc)
+        """
+
+        bs = y_true.shape[0]
+
+        y_true = y_true.view(bs, 1, -1)
+        y_pred = y_pred.view(bs, 1, -1)
+
+        tp = torch.sum(torch.mul(y_pred, y_true)) + self.eps
+        tn = torch.sum(torch.mul((1 - y_pred), (1 - y_true))) + self.eps
+        fp = torch.sum(torch.mul(y_pred, (1 - y_true))) + self.eps
+        fn = torch.sum(torch.mul((1 - y_pred), y_true)) + self.eps
+
+        numerator = torch.mul(tp, tn) - torch.mul(fp, fn)
+        denominator = torch.sqrt(torch.add(tp, fp) * torch.add(tp, fn) * torch.add(tn, fp) * torch.add(tn, fn))
+
+        mcc = torch.div(numerator.sum(), denominator.sum())
+        loss = 1.0 - mcc
+
+        return loss
+        
+        
+# =======================================================================
+# Soft BCE loss
+__all__ = ["SoftBCEWithLogitsLoss"]
+
+
+class SoftBCEWithLogitsLoss(nn.BCEWithLogitsLoss, base.Loss):
+
+    __constants__ = [
+        "weight",
+        "pos_weight",
+        "reduction",
+        "ignore_index",
+        "smooth_factor",
+    ]
+
+    def __init__(
+        self,
+        weight: Optional[torch.Tensor] = None,
+        ignore_index: Optional[int] = -100,
+        reduction: str = "mean",
+        smooth_factor: Optional[float] = None,
+        pos_weight: Optional[torch.Tensor] = None,
+    ):
+        """Drop-in replacement for torch.nn.BCEWithLogitsLoss with few additions: ignore_index and label_smoothing
+
+        Args:
+            ignore_index: Specifies a target value that is ignored and does not contribute to the input gradient.
+            smooth_factor: Factor to smooth target (e.g. if smooth_factor=0.1 then [1, 0, 1] -> [0.9, 0.1, 0.9])
+
+        Shape
+             - **y_pred** - torch.Tensor of shape NxCxHxW
+             - **y_true** - torch.Tensor of shape NxHxW or Nx1xHxW
+
+        Reference
+            https://github.com/BloodAxe/pytorch-toolbelt
+
+        """
+        super().__init__()
+        self.ignore_index = ignore_index
+        self.reduction = reduction
+        self.smooth_factor = smooth_factor
+        self.register_buffer("weight", weight)
+        self.register_buffer("pos_weight", pos_weight)
+
+    def forward(self, y_pred: torch.Tensor, y_true: torch.Tensor) -> torch.Tensor:
+        """
+        Args:
+            y_pred: torch.Tensor of shape (N, C, H, W)
+            y_true: torch.Tensor of shape (N, H, W)  or (N, 1, H, W)
+
+        Returns:
+            loss: torch.Tensor
+        """
+
+        if self.smooth_factor is not None:
+            soft_targets = (1 - y_true) * self.smooth_factor + y_true * (1 - self.smooth_factor)
+        else:
+            soft_targets = y_true
+
+        loss = torch.nn.functional.binary_cross_entropy_with_logits(
+            y_pred,
+            soft_targets,
+            self.weight,
+            pos_weight=self.pos_weight,
+            reduction="none",
+        )
+
+        if self.ignore_index is not None:
+            not_ignored_mask = y_true != self.ignore_index
+            loss *= not_ignored_mask.type_as(loss)
+
+        if self.reduction == "mean":
+            loss = loss.mean()
+
+        if self.reduction == "sum":
+            loss = loss.sum()
+
+        return loss
+        
+
+# =======================================================================        
+# Lavasez
+# from __future__ import print_function, division
+from typing import Optional
+
+import torch
+# import torch.nn.functional as F
+# from torch.nn.modules.loss import _Loss
+# from .constants import BINARY_MODE, MULTICLASS_MODE, MULTILABEL_MODE
+
+try:
+    from itertools import ifilterfalse
+except ImportError:  # py3k
+    from itertools import filterfalse as ifilterfalse
+
+__all__ = ["LovaszLoss"]
+
+
+def _lovasz_grad(gt_sorted):
+    """Compute gradient of the Lovasz extension w.r.t sorted errors
+    See Alg. 1 in paper
+    """
+    p = len(gt_sorted)
+    gts = gt_sorted.sum()
+    intersection = gts - gt_sorted.float().cumsum(0)
+    union = gts + (1 - gt_sorted).float().cumsum(0)
+    jaccard = 1.0 - intersection / union
+    if p > 1:  # cover 1-pixel case
+        jaccard[1:p] = jaccard[1:p] - jaccard[0:-1]
+    return jaccard
+
+
+def _lovasz_hinge(logits, labels, per_image=True, ignore=None):
+    """
+    Binary Lovasz hinge loss
+        logits: [B, H, W] Logits at each pixel (between -infinity and +infinity)
+        labels: [B, H, W] Tensor, binary ground truth masks (0 or 1)
+        per_image: compute the loss per image instead of per batch
+        ignore: void class id
+    """
+    if per_image:
+        loss = mean(
+            _lovasz_hinge_flat(*_flatten_binary_scores(log.unsqueeze(0), lab.unsqueeze(0), ignore))
+            for log, lab in zip(logits, labels)
+        )
+    else:
+        loss = _lovasz_hinge_flat(*_flatten_binary_scores(logits, labels, ignore))
+    return loss
+
+
+def _lovasz_hinge_flat(logits, labels):
+    """Binary Lovasz hinge loss
+    Args:
+        logits: [P] Logits at each prediction (between -infinity and +infinity)
+        labels: [P] Tensor, binary ground truth labels (0 or 1)
+        ignore: label to ignore
+    """
+    if len(labels) == 0:
+        # only void pixels, the gradients should be 0
+        return logits.sum() * 0.0
+    signs = 2.0 * labels.float() - 1.0
+    errors = 1.0 - logits * signs
+    errors_sorted, perm = torch.sort(errors, dim=0, descending=True)
+    perm = perm.data
+    gt_sorted = labels[perm]
+    grad = _lovasz_grad(gt_sorted)
+    loss = torch.dot(torch.nn.functional.relu(errors_sorted), grad)
+    return loss
+
+
+def _flatten_binary_scores(scores, labels, ignore=None):
+    """Flattens predictions in the batch (binary case)
+    Remove labels equal to 'ignore'
+    """
+    scores = scores.view(-1)
+    labels = labels.view(-1)
+    if ignore is None:
+        return scores, labels
+    valid = labels != ignore
+    vscores = scores[valid]
+    vlabels = labels[valid]
+    return vscores, vlabels
+
+
+# --------------------------- MULTICLASS LOSSES ---------------------------
+
+
+def _lovasz_softmax(probas, labels, classes="present", per_image=False, ignore=None):
+    """Multi-class Lovasz-Softmax loss
+    Args:
+        @param probas: [B, C, H, W] Class probabilities at each prediction (between 0 and 1).
+        Interpreted as binary (sigmoid) output with outputs of size [B, H, W].
+        @param labels: [B, H, W] Tensor, ground truth labels (between 0 and C - 1)
+        @param classes: 'all' for all, 'present' for classes present in labels, or a list of classes to average.
+        @param per_image: compute the loss per image instead of per batch
+        @param ignore: void class labels
+    """
+    if per_image:
+        loss = mean(
+            _lovasz_softmax_flat(*_flatten_probas(prob.unsqueeze(0), lab.unsqueeze(0), ignore), classes=classes)
+            for prob, lab in zip(probas, labels)
+        )
+    else:
+        loss = _lovasz_softmax_flat(*_flatten_probas(probas, labels, ignore), classes=classes)
+    return loss
+
+
+def _lovasz_softmax_flat(probas, labels, classes="present"):
+    """Multi-class Lovasz-Softmax loss
+    Args:
+        @param probas: [P, C] Class probabilities at each prediction (between 0 and 1)
+        @param labels: [P] Tensor, ground truth labels (between 0 and C - 1)
+        @param classes: 'all' for all, 'present' for classes present in labels, or a list of classes to average.
+    """
+    if probas.numel() == 0:
+        # only void pixels, the gradients should be 0
+        return probas * 0.0
+    C = probas.size(1)
+    losses = []
+    class_to_sum = list(range(C)) if classes in ["all", "present"] else classes
+    for c in class_to_sum:
+        fg = (labels == c).type_as(probas)  # foreground for class c
+        if classes == "present" and fg.sum() == 0:
+            continue
+        if C == 1:
+            if len(classes) > 1:
+                raise ValueError("Sigmoid output possible only with 1 class")
+            class_pred = probas[:, 0]
+        else:
+            class_pred = probas[:, c]
+        errors = (fg - class_pred).abs()
+        errors_sorted, perm = torch.sort(errors, 0, descending=True)
+        perm = perm.data
+        fg_sorted = fg[perm]
+        losses.append(torch.dot(errors_sorted, _lovasz_grad(fg_sorted)))
+    return mean(losses)
+
+
+def _flatten_probas(probas, labels, ignore=None):
+    """Flattens predictions in the batch"""
+    if probas.dim() == 3:
+        # assumes output of a sigmoid layer
+        B, H, W = probas.size()
+        probas = probas.view(B, 1, H, W)
+
+    C = probas.size(1)
+    probas = torch.movedim(probas, 1, -1)  # [B, C, Di, Dj, ...] -> [B, Di, Dj, ..., C]
+    probas = probas.contiguous().view(-1, C)  # [P, C]
+
+    labels = labels.view(-1)
+    if ignore is None:
+        return probas, labels
+    valid = labels != ignore
+    vprobas = probas[valid]
+    vlabels = labels[valid]
+    return vprobas, vlabels
+
+
+# --------------------------- HELPER FUNCTIONS ---------------------------
+def isnan(x):
+    return x != x
+
+
+def mean(values, ignore_nan=False, empty=0):
+    """Nanmean compatible with generators."""
+    values = iter(values)
+    if ignore_nan:
+        values = ifilterfalse(isnan, values)
+    try:
+        n = 1
+        acc = next(values)
+    except StopIteration:
+        if empty == "raise":
+            raise ValueError("Empty mean")
+        return empty
+    for n, v in enumerate(values, 2):
+        acc += v
+    if n == 1:
+        return acc
+    return acc / n
+
+
+class LovaszLoss(_Loss, base.Loss):
+    def __init__(
+        self,
+        mode: str = 'binary',  ## ----------->> pay attention <<---------------
+        per_image: bool = False,
+        ignore_index: Optional[int] = None,
+        from_logits: bool = True,
+    ):
+        """Lovasz loss for image segmentation task.
+        It supports binary, multiclass and multilabel cases
+        Args:
+            mode: Loss mode 'binary', 'multiclass' or 'multilabel'
+            ignore_index: Label that indicates ignored pixels (does not contribute to loss)
+            per_image: If True loss computed per each image and then averaged, else computed per whole batch
+        Shape
+              - **y_pred** - torch.Tensor of shape (N, C, H, W)
+              - **y_true** - torch.Tensor of shape (N, H, W) or (N, C, H, W)
+        Reference
+            https://github.com/BloodAxe/pytorch-toolbelt
+        """
+        assert mode in {BINARY_MODE, MULTILABEL_MODE, MULTICLASS_MODE}
+        super().__init__()
+
+        self.mode = mode
+        self.ignore_index = ignore_index
+        self.per_image = per_image
+
+    def forward(self, y_pred, y_true):
+
+        if self.mode in {BINARY_MODE, MULTILABEL_MODE}:
+            loss = _lovasz_hinge(y_pred, y_true, per_image=self.per_image, ignore=self.ignore_index)
+        elif self.mode == MULTICLASS_MODE:
+            y_pred = y_pred.softmax(dim=1)
+            loss = _lovasz_softmax(y_pred, y_true, per_image=self.per_image, ignore=self.ignore_index)
+        else:
+            raise ValueError("Wrong mode {}.".format(self.mode))
+        return loss
+
